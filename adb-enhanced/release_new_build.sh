4616ae0ea39db779466b03441f7febc4d709700a
Ashish Bhatia <ashishb@ashishb.net>
2018-07-22 16:45:56 -0700

    More fixes to the release script

--- adb-enhanced/release_new_build.sh
+++ adb-enhanced/release_new_build.sh
@@ -9,12 +9,14 @@ VERSION_FILENAME=${DIR}/version.txt
 cp ${DIR}/../adbe.py ${DIR}/adbe/ && 
   cp ${DIR}/../README.md ${DIR}/README.md &&
   # Open setup file to increment the version
-  echo -n "Next the editor will open ${VERSION_FILENAME}, increment the version number in it. Press enter to continue" &&
+  echo -n "Next the editor will open ${VERSION_FILENAME}, increment the version number in it. Press enter to continue:" &&
     # Wait for a keypress to ensure that user has seen the previous message
   read -n 1 -s &&
   vim ${VERSION_FILENAME} &&
   # One time setup
   python3 -m pip install --user --upgrade setuptools wheel twine &&
+  # Cleanup before creating the package
+  rm -r build/ dist/
   # Create the package. Reference: https://packaging.python.org/tutorials/packaging-projects/
   python3 setup.py sdist bdist_wheel &&
   # Commit to git before sending package upstream
@@ -25,5 +27,5 @@ cp ${DIR}/../adbe.py ${DIR}/adbe/ &&
   # Send the package upstream
   twine upload dist/* &&
   echo "Few mins later, check https://pypi.org/project/adb-enhanced/#history to confirm upload" &&
-  #Cleanup
+  # Cleanup
   rm -r build/ dist/
