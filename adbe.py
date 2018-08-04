2b602eb5e1369ceadd6047bb61d3fe774e72aa64
Ashish Bhatia <ashishb@ashishb.net>
2018-08-03 23:17:49 -0700

    Refactor print code out to output_helper

--- adbe.py
+++ adbe.py
@@ -11,6 +11,12 @@ import random
 import subprocess
 from urllib.parse import urlparse
 
+import output_helper
+# Don't import print_verbose explicitly since we call it via print_verbose
+# define in this module
+from output_helper import print_message, print_error, print_error_and_exit
+
+
 import docopt
 
 """
@@ -1161,34 +1167,9 @@ def _get_prop(property_name):
     return execute_adb_shell_command('getprop %s' % property_name)
 
 
-def print_message(message):
-    print(message)
-
-
-def print_error_and_exit(error_string):
-    print('%s%s%s' % (bcolors.FAIL, error_string, bcolors.ENDC))
-    quit(1)
-
-
-def print_error(error_string):
-    print('%s%s%s' % (bcolors.FAIL, error_string, bcolors.ENDC))
-
-
 def print_verbose(message):
     if _verbose:
-        print('%s%s%s' % (bcolors.WARNING, message, bcolors.ENDC))
-
-
-# Coloring approach inspired from https://stackoverflow.com/a/287944
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+        output_helper.print_verbose(message)
 
 
 if __name__ == '__main__':
---
+++ output_helper.py
@@ -0,0 +1,27 @@
+def print_message(message):
+    print(message)
+
+
+def print_error_and_exit(error_string):
+    print('%s%s%s' % (bcolors.FAIL, error_string, bcolors.ENDC))
+    quit(1)
+
+
+def print_error(error_string):
+    print('%s%s%s' % (bcolors.FAIL, error_string, bcolors.ENDC))
+
+
+def print_verbose(message):
+    print('%s%s%s' % (bcolors.WARNING, message, bcolors.ENDC))
+
+
+# Coloring approach inspired from https://stackoverflow.com/a/287944
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
