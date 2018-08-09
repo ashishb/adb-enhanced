c4d065d1c54bc0ebcb07061006fd275fce911df7
Ashish Bhatia <ashishb@ashishb.net>
2018-08-08 23:28:14 -0700

    Handle multiple device error more gracefully

--- adb_helper.py
+++ adb_helper.py
@@ -28,8 +28,9 @@ def execute_adb_command(adb_cmd, piped_into_cmd=None, ignore_stderr=False):
     if piped_into_cmd:
         print_verbose("Executing \"%s | %s\"" % (final_cmd, piped_into_cmd))
         ps1 = subprocess.Popen(final_cmd, shell=True, stdout=subprocess.PIPE,
-                               stderr=None if ignore_stderr is False else open(os.devnull, 'w'))
+                               stderr=subprocess.PIPE if ignore_stderr is False else open(os.devnull, 'w'))
         output = subprocess.check_output(piped_into_cmd, shell=True, stdin=ps1.stdout)
+        _check_for_more_than_one_device_error(ps1.stderr)
         if output is not None:
             output = output.decode('utf-8').strip()
         print_verbose(output)
@@ -37,7 +38,8 @@ def execute_adb_command(adb_cmd, piped_into_cmd=None, ignore_stderr=False):
     else:
         print_verbose("Executing \"%s\"" % final_cmd)
         ps1 = subprocess.Popen(final_cmd, shell=True, stdout=subprocess.PIPE,
-                               stderr=None if ignore_stderr is False else open(os.devnull, 'w'))
+                               stderr=subprocess.PIPE if ignore_stderr is False else open(os.devnull, 'w'))
+        _check_for_more_than_one_device_error(ps1.stderr)
         output = ''
         first_line = True
         for line in ps1.stdout:
@@ -48,3 +50,18 @@ def execute_adb_command(adb_cmd, piped_into_cmd=None, ignore_stderr=False):
                 output += '\n' + line.decode('utf-8').strip()
         print_verbose("Result is \"%s\"" % output)
         return output
+
+
+
+def _check_for_more_than_one_device_error(stderr):
+    if not stderr:
+        return
+    for line in stderr:
+        line = line.decode('utf-8').strip()
+        if line.find('error: more than one') != -1:
+            message = ''
+            message += 'More than one device/emulator are connected.\n'
+            message += 'Please select a device by providing the serial ID (-s parameter).\n'
+            message += 'You can list all connected devices/emulators via \"devices\" subcommand.'
+            print_error_and_exit(message)
+
