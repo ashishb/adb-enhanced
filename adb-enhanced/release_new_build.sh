8d85258d2e8873c5a540a71f396948d956453ab4
Ashish Bhatia <ashishb@ashishb.net>
2018-08-20 01:33:36 -0700

    More fixes to the release script

--- adb-enhanced/release_new_build.sh
+++ adb-enhanced/release_new_build.sh
@@ -4,16 +4,18 @@ set -e
 # Works on both Mac and GNU/Linux.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 VERSION_FILENAME=${DIR}/../version.txt
-SRC_FILES={adbe.py,output_helper.py,adb_helper.py,asyncio_helper.py,apksigner.jar,version.txt} 
+SRC_FILES=$(echo -n ${DIR}/../{adbe.py,output_helper.py,adb_helper.py,asyncio_helper.py,apksigner.jar,version.txt})
+DST_FILES=$(echo -n ${DIR}/adbe/{adbe.py,output_helper.py,adb_helper.py,asyncio_helper.py,apksigner.jar,version.txt})
+
 
 # Open setup file to increment the version
   echo -n "Next the editor will open ${VERSION_FILENAME}, increment the version number in it. Press enter to continue:" &&
-    # Copy the relevant files
-    cp ${DIR}/../${SRC_FILES} ${DIR}/adbe/ && 
-    cp ${DIR}/../README.md ${DIR}/README.md &&
   # Wait for a keypress to ensure that user has seen the previous message
   read -n 1 -s &&
   vim ${VERSION_FILENAME} &&
+  # Copy the relevant files
+  cp ${SRC_FILES} ${DIR}/adbe/ && 
+  cp ${DIR}/../README.md ${DIR}/README.md &&
   # One time setup
   python3 -m pip install --user --upgrade setuptools wheel twine &&
   # Cleanup before creating the package
@@ -27,7 +29,7 @@ SRC_FILES={adbe.py,output_helper.py,adb_helper.py,asyncio_helper.py,apksigner.ja
   python3 setup.py sdist bdist_wheel &&
   # Commit to git before sending package upstream
   git add ${DIR}/README.md \
-    ${DIR}/adbe/${SRC_FILES} ${DIR}/changelog.txt ${VERSION_FILENAME} &&
+    ${DST_FILES} ${DIR}/changelog.txt ${VERSION_FILENAME} &&
   git commit -m "Setup release $(cat $VERSION_FILENAME)" &&
   git tag $(cat $VERSION_FILENAME) &&
   git push origin master &&
