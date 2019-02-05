a4c0ca2b65e56e978ecf5641ca2cbd7d621b92ac
Ashish Bhatia <ashishb@ashishb.net>
2019-02-04 20:42:25 -0800

    release_new_build.sh -> release.py
    This BASH script was becoming complicated enough that I decided to
    migrate it to a python script

--- tests/lint_check_python3.sh
+++ tests/lint_check_python3.sh
@@ -12,4 +12,4 @@ set -eou pipefail
 # src/adbe.py:752:4: W0621: Redefining name 'screen_record_file_path_on_device' from outer scope (line 759) (redefined-outer-name)
 # src/adbe.py:756:8: W0601: Global variable 'screen_record_file_path_on_device' undefined at the module level (global-variable-undefined)
 # src/adbe.py:764:8: W0601: Global variable 'screen_record_file_path_on_device' undefined at the module level (global-variable-undefined)
-python3 -m pylint src/*.py tests/*.py release/setup.py --disable=R,C,W0603,E0602,W0511,W0406,W0621,W0601
+python3 -m pylint src/*.py tests/*.py release/setup.py release/release.py --disable=R,C,W0603,E0602,W0511,W0406,W0621,W0601
