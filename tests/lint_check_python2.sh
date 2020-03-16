bb4fece9317b7aec98de7be3c759156bc2705895
Ashish Bhatia <ashishb@ashishb.net>
2020-03-16 12:38:59 -0700

    Add Makefile
    Use Makefile for testing

--- tests/lint_check_python2.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eou pipefail
-
-# R = refactor
-# C = convention
-# W = warning
-
-# Don't check asyncio_helper for Python2 since it is written only for Python3
-python -m pylint src/adb_enhanced.py src/adb_helper.py src/main.py src/output_helper.py tests/*.py release/setup.py --disable=R,C,W
