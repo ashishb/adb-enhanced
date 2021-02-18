7d45326e214c696f8bc86ef2f85c577f5aa9bd56
root <60298999+SecTheTech@users.noreply.github.com>
2021-02-18 11:53:30 +0100

    If statements can be merged PTC-W0048

--- adbe/adb_enhanced.py
+++ adbe/adb_enhanced.py
@@ -17,6 +17,7 @@ import psutil
 if sys.version_info >= (3, 5):
     try:
         import asyncio_helper
+
         _ASYNCIO_AVAILABLE = True
     except ImportError:
         # This is to deal with python versions below 3.5
@@ -46,7 +47,6 @@ except ImportError:
     # noinspection PyUnresolvedReferences
     from output_helper import (print_message, print_error, print_error_and_exit,
                                print_verbose)
-
 
 _KEYCODE_BACK = 4
 _MIN_API_FOR_RUNTIME_PERMISSIONS = 23
@@ -80,6 +80,7 @@ def ensure_package_exists(func):
     def func_wrapper(package_name):
         _ensure_package_exists(package_name)
         return func(package_name)
+
     return func_wrapper
 
 
@@ -88,6 +89,7 @@ def ensure_package_exists2(func):
     def func_wrapper(package_name, arg2):
         _ensure_package_exists(package_name)
         return func(package_name, arg2)
+
     return func_wrapper
 
 
@@ -96,6 +98,7 @@ def ensure_package_exists3(func):
     def func_wrapper(package_name, arg2, arg3):
         _ensure_package_exists(package_name)
         return func(package_name, arg2, arg3)
+
     return func_wrapper
 
 
@@ -407,7 +410,7 @@ def _get_device_serials() -> [str]:
                 ('Unlock Device "%s" and give USB debugging access to ' +
                  'this PC/Laptop by unlocking and reconnecting ' +
                  'the device. More info about this device: "%s"\n') % (
-                     device_serial, device_info))
+                    device_serial, device_info))
         else:
             device_serials.append(device_serial)
     return device_serials
@@ -461,7 +464,7 @@ def _get_top_activity_data():
         app_name, activity_name = regex_result.group(1), regex_result.group(2)
         # If activity name is a short hand then complete it.
         if activity_name.startswith('.'):
-            activity_name = '%s%s' %(app_name, activity_name)
+            activity_name = '%s%s' % (app_name, activity_name)
         return app_name, activity_name
 
     print_error('Unable to extract activity name')
@@ -1115,7 +1118,7 @@ def get_standby_bucket(package_name):
     result = execute_adb_shell_command(cmd)
     if result is None:
         print_error_and_exit(_USER_PRINT_VALUE_UNKNOWN)
-    print_verbose('App standby bucket for \"%s\" is %s' %(
+    print_verbose('App standby bucket for \"%s\" is %s' % (
         package_name, _APP_STANDBY_BUCKETS.get(int(result), _USER_PRINT_VALUE_UNKNOWN)))
     print(_APP_STANDBY_BUCKETS.get(int(result), _USER_PRINT_VALUE_UNKNOWN))
 
@@ -1239,7 +1242,7 @@ def pull_file(remote_file_path, local_file_path, copy_ancillary=False):
             if not _file_exists(tmp_db_file):
                 continue
             if copy_ancillary:
-                pull_file(tmp_db_file, '%s-%s' %(local_file_path, suffix), copy_ancillary=True)
+                pull_file(tmp_db_file, '%s-%s' % (local_file_path, suffix), copy_ancillary=True)
             else:
                 print_error('File \"%s\" has an ancillary file \"%s\" which should be copied.\nSee %s for details'
                             % (remote_file_path, tmp_db_file,
@@ -1403,9 +1406,9 @@ def _get_permissions_info_above_api_23(app_info_dump):
             runtime_denied_permissions.append(permission)
     runtime_not_granted_permissions = list(filter(
         lambda p: p not in runtime_granted_permissions and
-        p not in runtime_denied_permissions and
-        p not in install_time_granted_permissions and
-        p not in install_time_denied_permissions, requested_permissions))
+                  p not in runtime_denied_permissions and
+                  p not in install_time_granted_permissions and
+                  p not in install_time_denied_permissions, requested_permissions))
 
     permissions_info_msg = ''
     permissions_info_msg += '\nPermissions:\n\n'
@@ -1476,7 +1479,8 @@ def perform_app_backup(app_name, backup_tar_file):
     # TODO: Add a check to ensure that the screen is unlocked
     password = '00'
     print_verbose('Performing backup to backup.ab file')
-    print_message('you might have to confirm the backup manually on your device\'s screen, enter \"%s\" as password...' % password)
+    print_message(
+        'you might have to confirm the backup manually on your device\'s screen, enter \"%s\" as password...' % password)
 
     def backup_func():
         # Create backup.ab
@@ -1625,7 +1629,7 @@ def enable_wireless_debug():
 
     # Check, that phone connected to wlan
     matching = re.findall(r"inet ([\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}).*wlan0$",
-            result, re.MULTILINE)
+                          result, re.MULTILINE)
     if matching is None or not matching:
         print_error_and_exit('Failed to switch device to wireless debug mode')
 
@@ -1713,7 +1717,7 @@ def print_notifications():
     for i, notification_record in enumerate(notification_records):
         output_for_this_notification = output.split(notification_record)[1]
         if i + 1 < len(notification_records):
-            output_for_this_notification = output_for_this_notification.split(notification_records[i+1])[0]
+            output_for_this_notification = output_for_this_notification.split(notification_records[i + 1])[0]
         notification_package = re.findall(r"pkg=(\S*)", notification_record)[0]
         titles = re.findall("android.title=(.*)", output_for_this_notification)
         if len(titles) > 0:
@@ -1727,11 +1731,12 @@ def print_notifications():
             notification_text = None
         notification_actions = []
         action_strings = re.findall(r"actions=\{(.*?)\n\}", output_for_this_notification, re.MULTILINE | re.DOTALL)
-        if len(action_strings) > 0:
-            if i+1 >= len(notification_records) or \
-                    output_for_this_notification.find(action_strings[0]) > output_for_this_notification.find(notification_records[i+1]):
-                for actions in action_strings[0].split('\n'):
-                    notification_actions += re.findall(r"\".*?\"", actions)
+        if len(action_strings) > 0 and (i + 1 >= len(notification_records) or
+                                        output_for_this_notification.find(
+                                            action_strings[0]) > output_for_this_notification.find(
+                    notification_records[i + 1])):
+            for actions in action_strings[0].split('\n'):
+                notification_actions += re.findall(r"\".*?\"", actions)
 
         print_message('Package: %s' % notification_package)
         if notification_title:
