af0efb428fdce208cc7ff5d1ecb88a757f9ffd60
Ashish Bhatia <ashishb@ashishb.net>
2023-10-13 20:23:09 -0700

    Increase minimum Python version requirement to 3.8

--- setup.py
+++ setup.py
@@ -4,49 +4,49 @@ import sys
 from setuptools import setup, find_packages
 
 _DIR_OF_THIS_SCRIPT = os.path.split(__file__)[0]
-_VERSION_FILE_NAME = 'version.txt'
-_VERSION_FILE_PATH = os.path.join(_DIR_OF_THIS_SCRIPT, 'adbe', _VERSION_FILE_NAME)
-_README_FILE_NAME = os.path.join('docs', 'README.rst')
+_VERSION_FILE_NAME = "version.txt"
+_VERSION_FILE_PATH = os.path.join(_DIR_OF_THIS_SCRIPT, "adbe", _VERSION_FILE_NAME)
+_README_FILE_NAME = os.path.join("docs", "README.rst")
 _README_FILE_PATH = os.path.join(_DIR_OF_THIS_SCRIPT, _README_FILE_NAME)
 
-with open(_VERSION_FILE_PATH, 'r') as fh:
+with open(_VERSION_FILE_PATH, "r") as fh:
     version = fh.read().strip()
 
 if os.path.exists(_README_FILE_NAME):
-    with open(_README_FILE_PATH, 'r') as fh:
+    with open(_README_FILE_PATH, "r") as fh:
         long_description = fh.read()
 else:
-    logging.warning('README file missing, final package will lack README: %s', _README_FILE_PATH)
-    long_description = ''
+    logging.warning("README file missing, final package will lack README: %s", _README_FILE_PATH)
+    long_description = ""
 
-packages = find_packages(exclude=['ez_setup', 'examples', 'tests'])
-if not packages or packages.index('adbe') == -1:
-    print('Failed to find adbe package')
+packages = find_packages(exclude=["ez_setup", "examples", "tests"])
+if not packages or packages.index("adbe") == -1:
+    print("Failed to find adbe package")
     sys.exit(1)
 
-setup(name='adb-enhanced',
+setup(name="adb-enhanced",
       version=version,
-      description='Swiss-army knife for Android testing and development',
+      description="Swiss-army knife for Android testing and development",
       long_description=long_description,
-      long_description_content_type='text/x-rst',
-      classifiers=['Intended Audience :: Developers'],
+      long_description_content_type="text/x-rst",
+      classifiers=["Intended Audience :: Developers"],
       # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='Android ADB developer',
-      author='Ashish Bhatia',
-      author_email='ashishb@ashishb.net',
-      url='https://github.com/ashishb/adb-enhanced',
-      license='Apache',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      keywords="Android ADB developer",
+      author="Ashish Bhatia",
+      author_email="ashishb@ashishb.net",
+      url="https://github.com/ashishb/adb-enhanced",
+      license="Apache",
+      packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
       include_package_data=True,
       zip_safe=True,
       install_requires=[
           # -*- Extra requirements: -*-
-          'docopt',
-          'psutil',
+          "docopt",
+          "psutil",
       ],
       entry_points={
           # -*- Entry points: -*-
-          'console_scripts': ['adbe=adbe.main:main'],
+          "console_scripts": ["adbe=adbe.main:main"],
       },
-      python_requires='>=3.6'
-      )
+      python_requires=">=3.8"
+)
