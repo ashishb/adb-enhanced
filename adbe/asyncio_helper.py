72a0c64f093b9102bd2b40ce5c663ea980863620
Ashish Bhatia <ashishb@ashishb.net>
2026-07-09 16:12:36 -0700

    chore: code clean up in asyncio_helper

--- adbe/asyncio_helper.py
+++ adbe/asyncio_helper.py
@@ -8,21 +8,15 @@ T = TypeVar("T")
 
 # Executes method method_to_call for each argument in params_list and returns the result_list
 def execute_in_parallel(method_to_call: Callable[[Any], T], params_list: list[Any]) -> list[T]:
-    result_list: list[T] = []
     num_workers = 50
-    loop = asyncio.get_event_loop()
 
-    async def _list_debug_apps_async(params_list2: list[Any]) -> None:
+    async def _run() -> list[T]:
         with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
-            loop = asyncio.get_event_loop()
+            loop = asyncio.get_running_loop()
             futures = [
-                loop.run_in_executor(
-                    executor,
-                    method_to_call,
-                    param) for param in params_list2
+                loop.run_in_executor(executor, method_to_call, param)
+                for param in params_list
             ]
+            return list(await asyncio.gather(*futures))
 
-            result_list.extend(await asyncio.gather(*futures))
-
-    loop.run_until_complete(_list_debug_apps_async(params_list))
-    return result_list
+    return asyncio.run(_run())
